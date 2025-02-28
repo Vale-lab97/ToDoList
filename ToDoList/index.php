c13203e3b198f6e00f1fdaad59e5d2ce080873db
Valeria <80955136+Vale-lab97@users.noreply.github.com>
2025-03-01 00:02:26 +0100

    Add files via upload

---
+++ ToDoList/index.php
@@ -0,0 +1,55 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="style.css" rel="stylesheet">
+    <title>To Do List</title>
+</head>
+<body>
+    <h1 class="title-todolist">To-Do List</h1>
+        <div class="container">
+            
+            <div class="todo">
+                <input class="todo-value" type="text" placeholder="Inserisci il tuo promemoria">
+                <input class="todo-checkbox" type="checkbox">
+            </div>
+
+            <div class="todo">
+                <input class="todo-value" type="text" placeholder="Inserisci il tuo promemoria">
+                <input class="todo-checkbox" type="checkbox">
+            </div>
+
+
+            <div class="todo">
+                <input class="todo-value" type="text" placeholder="Inserisci il tuo promemoria">
+                <input class="todo-checkbox" type="checkbox">
+            </div>
+
+            <div class="todo">
+                <input class="todo-value" type="text" placeholder="Inserisci il tuo promemoria">
+                <input class="todo-checkbox" type="checkbox">
+            </div>
+
+            
+            <div class="todo">
+                <input class="todo-value" type="text" placeholder="Inserisci il tuo promemoria">
+                <input class="todo-checkbox" type="checkbox">
+            </div>
+
+            
+            <div class="todo">
+                <input class="todo-value" type="text" placeholder="Inserisci il tuo promemoria">
+                <input class="todo-checkbox" type="checkbox">
+            </div>
+            
+        </div>
+    
+    
+</body>
+</html>
+
